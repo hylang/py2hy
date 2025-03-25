ef9410a6d2e9b339e92183f7f683fe8df63212fe
Kodi Arfer <git@arfer.net>
2025-03-25 11:47:37 -0400

    Require Hyrule 1.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
         'Source Code': 'https://github.com/hylang/py2hy'},
     install_requires = [
         'hy >= 1.1.0',
-        'hyrule'],
+        'hyrule >= 1'],
     packages = setuptools.find_packages(),
     package_data = dict(py2hy = [
         str(p.relative_to('py2hy'))
