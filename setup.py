bce28f712c0df33d98c733a9d2e89811943f96bb
Kodi Arfer <git@arfer.net>
2026-01-14 13:22:12 -0500

    Revert "Set versioning for the new release."
    This reverts commit a921ad95b44c4b3913ed9097d50f3a0e603a49d5.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name = 'py2hy',
-    version = '0.2.1',
+    version = '0.0.0',
     author = 'Kodi B. Arfer',
     description = 'Translate Python code to Hy code',
     long_description = Path('README.rst').read_text(),
